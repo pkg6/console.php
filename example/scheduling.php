dfbd19cd4ef6b662656773c234121f2a89fc118d
zhiqiangwang <zhiqiangwang@aliyun.com>
2023-11-13 14:09:57 +0800

    修改example/scheduling.php 运行schedule:work 出错

--- example/scheduling.php
+++ example/scheduling.php
@@ -1,7 +1,20 @@
 <?php
+
 use Pkg6\Console\Scheduling\Schedule;
 use Pkg6\Console\Scheduling\ScheduleConsole;
-require 'vendor/autoload.php';
+
+$autoloadFile = [
+    __DIR__ . '/../../../autoload.php',
+    __DIR__ . '/../../vendor/autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . 'vendor/autoload.php',
+];
+foreach ($autoloadFile as $file) {
+    if (file_exists($file)) {
+        require $file;
+        break;
+    }
+}
 
 class ConsoleScheduling extends ScheduleConsole
 {
@@ -22,4 +35,6 @@ class ConsoleScheduling extends ScheduleConsole
 }
 
 define("BASE_PATH", __DIR__);
+//使用schedule:work 必须设置你的入口文件，否则就根目录下consoles作为执行文件
+define("CONSOLE_NAME", __DIR__ . "/scheduling.php");
 (new ConsoleScheduling)->handle();
